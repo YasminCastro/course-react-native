6c92df47bf25602d01137e113fe28344bd140eee
Yasmin <yasminsdcastro@gmail.com>
2023-04-06 19:00:49 -0300

    feat: components

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,19 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { Logo } from "../../components/logo";
 
 export function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Pagina Home!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Logo />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "red",
-    height: 400,
+    flex: 1,
+    backgroundColor: "#f3f9ff",
+    paddingTop: 36,
+    paddingStart: 14,
   },
 });
