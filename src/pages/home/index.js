6ce57aaeeaed83e18a9903a657c24d1f522ab3ee
Yasmin <yasminsdcastro@gmail.com>
2023-04-11 16:57:25 -0300

    feat: input

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,29 @@
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { Logo } from "../../components/logo";
 
 export function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <Logo />
+      <Text style={styles.title}>Econtre a receita</Text>
+      <Text style={styles.title}>que combina com você</Text>
+      <View style={styles.form}>
+        <TextInput
+          placeholder="Digite o nome da comida..."
+          style={styles.input}
+        />
+        <TouchableOpacity>
+          <Ionicons name="search" size={28} color="#4CBE6C" />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -15,5 +34,28 @@ const styles = StyleSheet.create({
     backgroundColor: "#f3f9ff",
     paddingTop: 36,
     paddingStart: 14,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "#0e0e0e",
+  },
+  form: {
+    backgroundColor: "#FFF",
+    width: "100%",
+    borderRadius: 16,
+    marginBottom: 16,
+    marginTop: 16,
+    borderWidth: 1,
+    borderColor: "#ECECEC",
+    paddingLeft: 8,
+    paddingRight: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  input: {
+    width: "90%",
+    height: 54,
   },
 });
