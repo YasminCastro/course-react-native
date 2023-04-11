4efe33ce073fccef23764c2ceaaa17e16fa6f334
Yasmin <yasminsdcastro@gmail.com>
2023-04-11 17:53:37 -0300

    feat: header

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,6 +1,7 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import { useRoute, useNavigation } from "@react-navigation/native";
 import { useLayoutEffect } from "react";
+import { Entypo } from "@expo/vector-icons";
 
 export function Detail() {
   const route = useRoute();
@@ -11,6 +12,11 @@ export function Detail() {
       title: route.params?.food
         ? route.params.food.name
         : "Detalhes da receita",
+      headerRight: () => (
+        <Pressable onPress={() => console.log("TESTANDO")}>
+          <Entypo name="heart" size={28} color="#FF4141" />
+        </Pressable>
+      ),
     });
   }, [navigation, route.params?.food]);
 
